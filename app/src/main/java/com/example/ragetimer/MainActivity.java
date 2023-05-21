0c2a34ae2098dfb6deeaf8cafe26e0e96faa001c
andrew-of-ulster <amullan1010@gmail.com>
2023-05-21 01:07:03 +0100

    Progress Bad half working

--- app/src/main/java/com/example/ragetimer/MainActivity.java
+++ app/src/main/java/com/example/ragetimer/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,21 +15,30 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
 
+    private ProgressBar mProgressBar;
+    private TextView mLoadingText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar2);
+
         button= (Button) findViewById(R.id.button);
         textView= (TextView) findViewById(R.id.textView);
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
-                int totalTime=30000;
+                int totalTime=10000;
                 int interval=1000;
                 new CountDownTimer(totalTime, interval){
                     public void onTick(long millisUntilFinished){
+                        int remainingtime = totalTime - counter;
                         textView.setText(String.valueOf((totalTime/1000)-counter));
+                        mProgressBar.setProgress((totalTime/1000)-counter);
                         counter++;
                     }
                     public  void onFinish(){
